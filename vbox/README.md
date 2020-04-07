9c07871db88824aa3245a33a26461bc5c8aeae6e
icebowl <icebowl1967@gmail.com>
2020-04-07 12:28:17 -0400

    AA

--- vbox/README.md
+++ vbox/README.md
@@ -1,1 +1,4 @@
  dnf install tar bzip2 kernel-devel-$(uname -r) kernel-headers perl gcc make elfutils-libelf-devel 
+(this worked) dnf install binutils kernel-devel kernel-headers libgomp make patch gcc glibc-headers glibc-devel dkms -y^C
+[root@e4 /]# 
+
--- vbox/headers.sh
+++ vbox/headers.sh
@@ -1,1 +1,4 @@
 yum install dkms gcc make kernel-devel bzip2 binutilsy patch libgomp glibc-headers glibc-devel kernel-headers -y
+ dnf install binutils kernel-devel kernel-headers libgomp make patch gcc glibc-headers glibc-devel dkms -y^C
+[root@e4 /]# 
+
